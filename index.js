51297f29634b3d8e6d7b66670bd111cbe0ac3975
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-05-23 09:56:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -85,28 +85,22 @@ const redLightDiv = document.getElementById("circle-red");
 const yellowLightDiv = document.getElementById("circle-yellow");
 const greenLightDiv = document.getElementById("circle-green");
 
-(async function() {
-
-    let redLightOn = new Promise((res, rej) => {
 setTimeout(function() {
     resolve(redLightDiv.classList.toggle('hidden'));
     }, 2000);
-    
-    let result = await redLightOn;
-}
-//let redLightOff = redLightDiv.classList.toggle('hidden');
+ 
+redLightDiv.classList.toggle('hidden');
 
-//}
 
-// setTimeout(function() {
-//     yellowLightDiv.classList.toggle('hidden');
-//     }, 3000);
+setTimeout(function() {
+    yellowLightDiv.classList.toggle('hidden');
+    }, 3000);
 
-// yellowLightDiv.classList.toggle('hidden');
+yellowLightDiv.classList.toggle('hidden');
 
-// setTimeout(function() {
-//     greenLightDiv.classList.toggle('hidden');
-//     }, 4000);    
+setTimeout(function() {
+    greenLightDiv.classList.toggle('hidden');
+    }, 4000);    
 
-// greenLightDiv.classList.toggle('hidden');
+greenLightDiv.classList.toggle('hidden');
 
